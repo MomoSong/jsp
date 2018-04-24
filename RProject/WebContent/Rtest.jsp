c4ad965ba8e1da875638d5e03151f15d60d9cd45
401PC-D5\hong <johnmor78@gmail.com>
2018-04-24 11:16:22 +0900

    no message

---
+++ RProject/WebContent/Rtest.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<iframe src="http://127.0.0.1:7776/mpg.html"></iframe>
+</body>
+</html>
---
+++ ch21memberTeacher/src/com/webjjang/filter/AuthLoginCheckFilter.java
@@ -0,0 +1,51 @@
+package com.webjjang.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * Servlet Filter implementation class AuthLoginCheckFilter
+ */
+public class AuthLoginCheckFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public AuthLoginCheckFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		System.out.println(getClass().getName()+".doFilter()");
+		// 1. 로그인 체크
+		// 2. 권한 체크
+
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
